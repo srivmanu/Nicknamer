9a0a1a042d0b5f8f0cb1cd2b903ff73f4136635d
Manu <srivmanu@gmail.com>
2018-10-06 13:01:00 -0500

    Initial commit

---
+++ app/src/main/java/friday/nicknamer/Nicknamer.java
@@ -0,0 +1,13 @@
+package friday.nicknamer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nicknamer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nicknamer);
+    }
+}
