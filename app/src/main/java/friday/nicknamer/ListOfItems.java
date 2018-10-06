de8e341984bd5816b172360500c248602dd2af67
Manu <srivmanu@gmail.com>
2018-10-06 15:17:13 -0500

    -3:17pm

---
+++ app/src/main/java/friday/nicknamer/ListOfItems.java
@@ -0,0 +1,81 @@
+package friday.nicknamer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ListOfItems.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ListOfItems#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListOfItems extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ListOfItems() {
+        // Required empty public constructor
+    }
+
+    public static ListOfItems newInstance() {
+        ListOfItems fragment = new ListOfItems();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(NicknamerAction action);
+    }
+}
---
+++ app/src/main/java/friday/nicknamer/NicknamerAction.java
@@ -0,0 +1,11 @@
+package friday.nicknamer;
+
+/**
+ * Created by srivmanu on 06-Oct-18.
+ * All versions of this code are expected to be inspection only.
+ * If you are planning to Submit this to ply store, God bless your Soul.
+ */
+enum NicknamerAction {
+    backPressed,
+    nextPressed
+}
